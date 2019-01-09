af264b1a2891077c66b9305bb9a3ed397e82b590
Sebastian Johansson <sebben.97@hotmail.com>
2019-01-09 12:52:36 +0100

    added onclick function to the save button.

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
 
     function showHoroscope(){
-        $.ajax({
+        $.ajax(
+            {
             method: 'GET',
             url: "php/viewHoroscope.php",
-        })
+            }
+        )
         .done(function(data){
             $('.result').html(data);
         })
     };
     showHoroscope();
+
+    $("#save").on("click", function(){ saveHoroscope(); });
+    
+    //Function to save your birthdate via AJAX using the POST method, if sucessfull, show your sign.
+    function saveHoroscope(){
+        var birthDate = $("#date").val();
+        if (birthDate !== ""){
+            $.ajax(
+                {
+                    method: 'POST',
+                    url: 'php/addHoroscope.php',
+                    data: {
+                        birthDate: birthDate
+                    }
+                }
+            )
+            .done(function(data){
+                showHoroscope();
+                console.log(data);
+            })
+        }
+    }
 });
