7dede1411c631ca30cc175d5a74fcf7f5df70c75
Sebastian Johansson <sebben.97@hotmail.com>
2019-01-14 10:39:09 +0100

    bugg fixes

--- php/addHoroscope.php
+++ php/addHoroscope.php
@@ -1,14 +1,18 @@
 <?php
     session_start();
 
-    include "allHoroscopes.php";
-
+    
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $month = substr($_POST['birthDate'], 0, 1);
-        $day = substr($_POST['birthDate'], 2, 2);
+        include "allHoroscopes.php";
+        
+        $month = substr($_POST['birthDate'], 2, 2);
+        $day = substr($_POST['birthDate'], 4, 2);
 
         if(!isset($_SESSION['yourHoroscope'])){
             $_SESSION['yourHoroscope'] = myHoroscope($month, $day);
+            echo "true";
+        }else {
+            echo "false";
         }
     }
 ?>
